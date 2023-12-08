7d1016baa0832e0c24d83244bd08ae0eb64b2491
manishkrj <134487043+manishkrj@users.noreply.github.com>
2023-12-08 18:59:04 +0530

    removed update

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,14 +1,8 @@
 import { createNextRouteHandler } from "uploadthing/next";
-import { NextApiRequest, NextApiResponse } from "next";
+ 
 import { ourFileRouter } from "./core";
-
-// Create Next.js route handler
-const handler = createNextRouteHandler({
+ 
+// Export routes for Next App Router
+export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
 });
-
-export default handler;
-
-// Optional: You can export other variables or functions if needed
-export const maxDuration = 10; // This function can run for a maximum of 5 seconds
-export const dynamic = 'force-dynamic';
