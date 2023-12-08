8c392d8b8eedeb05dbf870a0f698e7c593594724
manishkrj <134487043+manishkrj@users.noreply.github.com>
2023-12-09 01:27:22 +0530

    implement all features

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -9,13 +9,13 @@ async function Page(){
         const userInfo = await fetchUser(user.id);
         if(userInfo?.onboarded)redirect("/")
         const userData = {
-            id: user?.id,
+            id: user.id,
             objectId: userInfo?._id,
-            username: userInfo?.username || user?.username,
-            name: userInfo?.name || user?.firstName || "",
-            bio: userInfo?.bio || "",
-            image: userInfo?.image || user?.imageUrl,
-        }
+            username: userInfo ? userInfo?.username : user.username,
+            name: userInfo ? userInfo?.name : user.firstName ?? "",
+            bio: userInfo ? userInfo?.bio : "",
+            image: userInfo ? userInfo?.image : user.imageUrl,
+          };
         return (
             <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
                 <h1 className="head-text">Onboarding</h1>
--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,4 @@
+import { formatDateString } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -70,7 +71,7 @@ const ThreadCard = ({
                                 <Image src="/assets/repost.svg" alt="repost" width={24} height={24} className="cursor-pointer object-contain" />
                                 <Image src="/assets/share.svg" alt="share" width={24} height={24} className="cursor-pointer object-contain" />
                             </div>
-
+                            
                             {isComment && comments.length>0 && (
                                 <Link href={`/thread/${id}`}>
                                     <p className="mt-1 text-subtle-medium text-gray-1">{comments.length} replies </p>
@@ -82,6 +83,20 @@ const ThreadCard = ({
 
                 </div>
             </div>
+            {!isComment && community &&(
+                <Link href={`/communities/${community.id}`} className="mt-5 flex items-center">
+                    <p className="text-subtle-medium text-gray-1">
+                        {formatDateString(createdAt)}{" "} - {community.name} Community
+                    </p>
+                    <Image
+                        src={community.image}
+                        alt={community.name}
+                        width={14}
+                        height={15}
+                        className="ml-1 rounded-full object-cover"
+                    />
+                </Link>
+            )}
         </article>
     )
 }
