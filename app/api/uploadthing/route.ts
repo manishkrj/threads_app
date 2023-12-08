23ccf278a9277d6bbe9415b180024448cb087225
manishkrj <134487043+manishkrj@users.noreply.github.com>
2023-12-08 20:23:42 +0530

    removed route

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,54 +1,9 @@
 import { createNextRouteHandler } from "uploadthing/next";
+ 
 import { ourFileRouter } from "./core";
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-
-// Create Next.js route handler
-const { GET: originalGET } = createNextRouteHandler({
+ 
+// Export routes for Next App Router
+export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
 });
 
-export const runtime = 'edge'; // 'nodejs' is the default
-
-// Combine logic from both GET implementations
-export async function GET(request: NextRequest): Promise<Response> {
-  try {
-    // Execute both original and custom logic concurrently
-    const [originalResponse, customResponse] = await Promise.all([
-      originalGET(request),
-      getNextResponse(request),
-    ]);
-
-    // Merge or prioritize the responses as needed
-    const mergedResponse = {
-      ...originalResponse,
-      ...customResponse,
-      // Add any additional merging logic if needed
-    };
-
-    return mergedResponse;
-  } catch (error) {
-    // Handle errors and return an appropriate response
-    console.error("Error:", error);
-
-    // Return a valid error response conforming to Response type
-    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-}
-
-// Function to perform custom logic and return NextResponse
-async function getNextResponse(request: NextRequest): Promise<Response> {
-  return NextResponse.json(
-    {
-      body: request.body,
-      query: request.nextUrl.search,
-      cookies: request.cookies.getAll(),
-    },
-    {
-      status: 200,
-    },
-  );
-}
