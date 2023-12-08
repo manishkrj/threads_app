822db8523614ee17f6e11194b01f8fbfb788e5a7
manishkrj <134487043+manishkrj@users.noreply.github.com>
2023-12-08 18:38:25 +0530

    route.ts

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,8 +1,17 @@
 import { createNextRouteHandler } from "uploadthing/next";
- 
+
 import { ourFileRouter } from "./core";
- 
+
 // Export routes for Next App Router
 export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
 });
+export const maxDuration = 5; // This function can run for a maximum of 5 seconds
+export const dynamic = 'force-dynamic';
+
+// This is the correct definition for the GET function
+export function handleGET(request: Request) {
+  return new Response('Vercel', {
+    status: 200,
+  });
+}
