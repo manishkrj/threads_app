a29101650c951647b816b391cc0df11d3812a6ab
manishkrj <134487043+manishkrj@users.noreply.github.com>
2023-12-08 19:20:36 +0530

    updated again2

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,4 +1,4 @@
-import { createNextRouteHandler} from "uploadthing/next";
+import { createNextRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
 import { NextApiRequest, NextApiResponse } from "next";
 
