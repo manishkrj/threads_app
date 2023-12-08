3f14ffda654d9677295a9317ceda4497083ee5e4
manishkrj <134487043+manishkrj@users.noreply.github.com>
2023-12-08 18:52:41 +0530

    update route3

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -10,5 +10,5 @@ const handler = createNextRouteHandler({
 export default handler;
 
 // Optional: You can export other variables or functions if needed
-export const maxDuration = 5; // This function can run for a maximum of 5 seconds
+export const maxDuration = 10; // This function can run for a maximum of 5 seconds
 export const dynamic = 'force-dynamic';
