032b2d7217ce7baec61ced62bdbd0811cd5a5599
manishkrj <134487043+manishkrj@users.noreply.github.com>
2023-12-08 20:02:48 +0530

    updated again5

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,8 +1,38 @@
 import { createNextRouteHandler } from "uploadthing/next";
- 
 import { ourFileRouter } from "./core";
- 
-// Export routes for Next App Router
-export const { GET, POST } = createNextRouteHandler({
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+// Create Next.js route handler
+const { GET: originalGET } = createNextRouteHandler({
   router: ourFileRouter,
 });
+
+export const runtime = 'edge'; // 'nodejs' is the default
+
+// Combine logic from both GET implementations
+export async function GET(request: NextRequest) {
+  // Original logic from createNextRouteHandler
+  const originalResponse = await originalGET(request);
+
+  // Your additional logic from the second implementation
+  const customResponse = NextResponse.json(
+    {
+      body: request.body,
+      query: request.nextUrl.search,
+      cookies: request.cookies.getAll(),
+    },
+    {
+      status: 200,
+    },
+  );
+
+  // Merge or prioritize the responses as needed
+  const mergedResponse = {
+    ...originalResponse,
+    ...customResponse,
+    // Add any additional merging logic if needed
+  };
+
+  return mergedResponse;
+}
