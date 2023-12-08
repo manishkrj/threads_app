d72c3535923a8b74a4a073c3e88757d248a03d6c
manishkrj <134487043+manishkrj@users.noreply.github.com>
2023-12-08 20:13:18 +0530

    updated again6

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -12,27 +12,36 @@ export const runtime = 'edge'; // 'nodejs' is the default
 
 // Combine logic from both GET implementations
 export async function GET(request: NextRequest) {
-  // Original logic from createNextRouteHandler
-  const originalResponse = await originalGET(request);
+  try {
+    // Original logic from createNextRouteHandler
+    const originalResponse = await originalGET(request);
 
-  // Your additional logic from the second implementation
-  const customResponse = NextResponse.json(
-    {
-      body: request.body,
-      query: request.nextUrl.search,
-      cookies: request.cookies.getAll(),
-    },
-    {
-      status: 200,
-    },
-  );
+    // Your additional logic from the second implementation
+    const customResponse = NextResponse.json(
+      {
+        body: request.body,
+        query: request.nextUrl.search,
+        cookies: request.cookies.getAll(),
+      },
+      {
+        status: 200,
+      },
+    );
 
-  // Merge or prioritize the responses as needed
-  const mergedResponse = {
-    ...originalResponse,
-    ...customResponse,
-    // Add any additional merging logic if needed
-  };
+    // Merge or prioritize the responses as needed
+    const mergedResponse = {
+      ...originalResponse,
+      ...customResponse,
+      // Add any additional merging logic if needed
+    };
 
-  return mergedResponse;
+    return mergedResponse;
+  } catch (error) {
+    // Handle errors and return an appropriate response
+    console.error("Error:", error);
+    return {
+      body: { error: "Internal Server Error" },
+      status: 500,
+    };
+  }
 }
