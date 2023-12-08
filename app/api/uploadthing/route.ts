3e171af6e2f8e52096cd8c59ffd280aba9ace552
manishkrj <134487043+manishkrj@users.noreply.github.com>
2023-12-08 18:51:10 +0530

    update route2

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -2,14 +2,13 @@ import { createNextRouteHandler } from "uploadthing/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import { ourFileRouter } from "./core";
 
-// Export routes for Next App Router
-export const { GET, POST } = createNextRouteHandler({
+// Create Next.js route handler
+const handler = createNextRouteHandler({
   router: ourFileRouter,
 });
+
+export default handler;
+
+// Optional: You can export other variables or functions if needed
 export const maxDuration = 5; // This function can run for a maximum of 5 seconds
 export const dynamic = 'force-dynamic';
-
-// Implement the GET route handler
-export default async function handleGET(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: 'Vercel' });
-}
