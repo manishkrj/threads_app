0500ff04f23b6c6ec1db182f09e522a38843d02d
manishkrj <134487043+manishkrj@users.noreply.github.com>
2023-12-08 19:24:14 +0530

    updated again3

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 // Function to create a timeout handler
 const createTimeoutHandler = (
-  handler: (req: NextApiRequest, res: NextApiResponse) => Promise<void>,
+  handler: (req: NextApiRequest | Request, res: NextApiResponse) => Promise<void>,
   maxDuration: number
 ) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
@@ -29,14 +29,14 @@ const { GET: originalGET, POST: originalPOST } = createNextRouteHandler({
 const maxDuration = 10;
 
 // Wrap the entire GET handler with the timeout function
-export const GET = createTimeoutHandler(async (request: NextApiRequest, response: NextApiResponse) => {
+export const GET = createTimeoutHandler(async (request: NextApiRequest | Request, response: NextApiResponse) => {
   // Your Vercel-specific logic here
   // For example, responding with 'Vercel'
   response.status(200).json({ message: 'Vercel' });
 }, maxDuration);
 
 // Wrap the entire POST handler with the timeout function
-export const POST = createTimeoutHandler(async (request: NextApiRequest, response: NextApiResponse) => {
+export const POST = createTimeoutHandler(async (request: NextApiRequest | Request, response: NextApiResponse) => {
   // Your Vercel-specific logic here for POST
   // For example, responding with 'Vercel POST'
   response.status(200).json({ message: 'Vercel POST' });
